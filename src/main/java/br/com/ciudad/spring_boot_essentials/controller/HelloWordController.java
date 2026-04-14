bc1385ffde584dbeecd2ea481b02e456592e74c1
eduardo Ciudad <eduardociudadfigueredo@gmail.com>
2026-04-14 18:46:46 -0300

    feat: create basic endpoints for mini course project

--- src/main/java/br/com/ciudad/spring_boot_essentials/controller/HelloWordController.java
+++ src/main/java/br/com/ciudad/spring_boot_essentials/controller/HelloWordController.java
@@ -1,17 +1,38 @@
 package br.com.ciudad.spring_boot_essentials.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/hello")
 public class HelloWordController {
 
-    @GetMapping
-    public ResponseEntity<String> helloworld (){
+    @GetMapping(value = {"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public String get(@PathVariable ("id") String id){ // jeito diferente de dar um return no endpoint
+        return "HEllo World" + id;// passando  o parametro por patch verbol
+    }
+
+    /*@GetMapping
+    public ResponseEntity<String> helloworld1 (){
         return ResponseEntity.ok("Hello World");
     }
+
+    nao pode ter mais de 1 endpoinnt indo para a mesma url porque da conflito erro405
+    */
+
+    @GetMapping("/novo")
+    @ResponseStatus(HttpStatus.OK)
+    public String getnovo(@RequestParam(value = "name", required = false) String name){
+        return "HEllo World" + name;
+    } // assim pode pq mudou a url
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String post(@RequestBody String nome ){
+        return "HEllo World Post!" + nome;
+    }
+
 }
